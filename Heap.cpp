b90784d2eafd9751b879ad8820bdceb110f0e1c1
Rimil.mandrita <31147874+Rimilmandrita@users.noreply.github.com>
2018-04-16 22:38:10 +0530

    Update Heap.cpp

--- Heap.cpp
+++ Heap.cpp
@@ -1,128 +1,253 @@
 #include<iostream>
 using namespace std;
 
-
-//maxheap class, using array
-
-
-class MaxHeap
-{
-  public:
-  int maxheap[100];
-  int size;
-  MaxHeap()
-  { 
-    size=0;
+class node{
+     public:
+     node *parent;
+     node *left;
+     node *right;
+     int data;//Rimilmandrita Ghosh, 1711106
+    node(){
+     parent=left=right=NULL;
+     data=0;
     }
-//insert function inserts the element at the end,and then reheapifies it upwards
-  void insert(int key)
-  {
-  int i=size++;
-  maxheap[i]=key;
-  while(i!=0)
-  {
-    int p=(i-1)/2;
-    if(maxheap[p]>=key)
-      break;
-    else
-    {
-       int temp=maxheap[p];
-       maxheap[p]=maxheap[i];
-       maxheap[i]=teMaxHeap()
-  { 
-    size=0;
-  }
-mp;
-    }
-    i=p;
-   }
-  }
-
-//1711106
-//Rimilmandrita Ghosh
-
-  //displays the array as it should be, since elements are arranged adhering to the properties of maximum heap
-  void display()
-  {
-   cout<<"\nTHE MAX-HEAP ELEMENTS ARE:";
-   for(int i=0;i<size;i++)
-   {
-     cout<<"\t"<<maxheap[i];
-   }
-  }
- //takes into consideration three cases, size=0, size=1, and size greater than 1
-  void remove()   //remove maximum element (i.e., root element)
-  {
-  if(size==0)
-  {
-     cout<<"\nTHE HEAP IS EMPTY !!! \n \n";
-  }
-  else if(size==1)
-  {
-    size--;
-  }
-    
-  //when size>1
-  else
-  {
-    int i=0;
-    maxheap[i]=maxheap[size-1];
-    size--;
-    
-    //if the current node has a children
-    while((2*i+1)+1<=size)
-    {
-       
-      //if it has two children
-      if((2*i+2)+1<=size)
-      {
-        int lc=maxheap[2*i+1];
-        int rc=maxheap[2*i+2];
-        int maxind=(lc>=rc)?(2*i+1):(2*i+2);
-        if(maxheap[i]<maxheap[maxind])
-        {
-          int temp=maxheap[i];
-          maxheap[i]=maxheap[maxind];
-          maxheap[maxind]=temp;
-          i=maxind;
-        }
-        
-      }
-      
-      //if it has only one child
-      else
-      {
-        if(maxheap[i]<maxheap[2*i+1])
-        {
-          int temp=maxheap[i];
-          maxheap[i]=maxheap[2*i+1];
-          maxheap[2*i+1]=temp;
-          i=2*i+1;
-        }
-        else
-          break;
-      }
-    }
-  }
- }
 };
 
-int main()
+class BStree{
+     public:
+     node *root;
+     int noofnodes,leaves;
+     int rngsrc;
+
+    BStree()
+    {
+    root=NULL;
+    }
+
+
+ void insert(int x){
+   if(searchNode(x)==NULL)
+   {node *temp=new node;
+   temp->data=x;
+   temp->parent=temp->left=temp->right=NULL;
+
+   if(root==NULL)
+   { root=temp;
+   }
+   else{ node *curr;
+         curr=root;
+        while(1){
+            if(x<curr->data){
+
+               if(curr->left==NULL){
+                  temp->parent=curr;
+                  curr->left=temp;
+               break;
+               }
+              else{curr=curr->left;}
+            }
+           else {
+                    if(curr->right==NULL)
+                       {
+                       temp->parent=curr;
+                       curr->right=temp;
+                     break;}
+                   else{curr=curr->right;}
+                  }
+
+        }
+     }}
+  else
+  cout<<"\nElement exists";
+ }
+//Rimilmandrita Ghosh, 1711106
+ void display(node *temp){
+
+      if(temp==NULL)
+         return;
+      display(temp->left);
+      cout<<"\t"<<temp->data;
+      display(temp->right);
+}
+
+node* searchNode(int dt)
 {
-  MaxHeap MH;
-  cout<<"\n \nEnter the number of elements you want to enter: ";
-  int n;
-  cin>>n;
-  cout<<"\nEnter the datas:\n";
-  for(int x=0;x<n;x++)
-  { 
-    int num;
-    cout<<"\nDATA"<<(x+1)<<": ";
-    cin>>num;
-    MH.insert(num);
+ node * check = root;
+ if(check == NULL)
+     return NULL;
+ else
+  while(true)
+  {
+   if(check->data == dt)
+   {
+     cout << "\nNode found.\n";
+     return check;
+   }
+   else if(check->data > dt)
+   {
+    if(check->left == NULL)
+      {
+
+       return NULL;
+      }
+    else
+       check = check->left;
+   }
+   else
+   {
+    if(check->right == NULL)
+    {
+
+      return NULL;
+    }
+    else
+      check = check->right;
+   }
+
   }
-  MH.display();
-  MH.remove();
-  MH.display();
-  return 0;
+
+}
+
+
+void deleteNode(int s)
+{
+
+  if(searchNode(s)==NULL)
+  {cout<<"Node not found \n";
+  }
+  else
+{
+
+    node*curr=searchNode(s);
+
+    // if this is leaf node
+    if(curr->left==NULL && curr->right==NULL)
+    { if(curr==root)
+        {root=NULL;
+        cout<<"Tree deleted \n";}
+      if(curr->parent->left==curr)
+        curr->parent->left=NULL;
+      else
+         curr->parent->right=NULL;
+     cout<<"Node deleted \n";
+    }
+    //if it has one single child
+    else if(curr->left==NULL || curr->right==NULL)
+    {if(curr==root)
+      {if(curr->left==NULL)
+        {root=curr->right;
+         curr->right->parent=NULL;
+        }
+       else
+        {root=curr->left;
+         curr->left->parent==NULL;
+        }
+        cout<<"Node deleted \n";
+      }
+     if(curr->left==NULL)
+      { curr->right->parent=curr->parent;
+        if(curr->parent->left==curr)
+          curr->parent->left=curr->right;
+        else
+          curr->parent->right=curr->right;
+      }
+      else
+      { curr->left->parent=curr->parent;
+        if(curr->parent->left==curr)
+           curr->parent->left=curr->left;
+        else
+           curr->parent->right=curr->left;
+      }
+      cout<<"Node deleted \n";
+    }
+   //if it has two children
+   else
+   {node* temp=curr->right;
+    while(temp->left!=NULL)
+     {temp=temp->left;}
+     //temp has the element we want to replace
+     int cons;
+     cons=temp->data;
+     deleteNode(cons);
+     curr->data=cons;
+
+ }}  }
+
+
+
+void counting (node* temp)
+{ if(temp==NULL)
+         return;
+       counting(temp->left);
+      noofnodes++;
+      if(temp->left==NULL && temp->right==NULL)
+      {leaves++;}
+      counting(temp->right);}
+
+
+
+ void rangeSearch(node* temp,int k1,int k2)
+ {  if(temp==NULL)
+         return;
+      rangeSearch(temp->left,k1,k2);
+      if(temp->data>=k1 && temp->data<=k2)
+      {cout<<"\t"<<temp->data;
+       rngsrc++;
+      }
+      rangeSearch(temp->right,k1,k2);
+ }
+
+
+//Rimilmandrita Ghosh
+
+
+ };
+
+ int main(){
+ BStree b;
+ cout<<"\nMENU";
+ cout<<"\n1.INSERT";
+ cout<<"\n2.DISPLAY";
+ cout<<"\n3.SEARCH";
+ cout<<"\n4.DELETE";
+ cout<<"\n5.RANGE SEARCH";
+ cout<<"\n6.COUNTING THE ELEMENTS";
+ cout<<"\n7.EXIT";
+
+int ch,x;
+do{
+  cout<<"\nenter your choice (1-7): ";
+  cin>>ch;
+  switch(ch){
+  case 1:cout<<"\nenter the data you want to enter :";
+         cin>>x;
+         b.insert(x);break;
+  case 2:b.display(b.root);break;
+  case 3:cout<<"\nenter the data you want to search :";
+         cin>>x;
+         node* temp;
+         temp=b.searchNode(x);
+         if(temp==NULL)
+         {cout<<"\nNOT FOUND";}break;
+case 4:cout<<"\nenter the data you want to delete :";
+         cin>>x;
+         b.deleteNode(x);break;
+  case 5:cout<<"\n Enter the limits 1 and 2:";
+         int k1,k2;
+         cin>>k1>>k2;
+         b.rngsrc=0;
+         cout<<"\n";
+         b.rangeSearch(b.root,k1,k2);
+         cout<<"\nThe number of elements are "<<b.rngsrc;
+         break;
+case 6:cout<<"\n The number of elements are ";
+       b.noofnodes=0;b.leaves=0;
+       b.counting(b.root);
+       cout<<b.noofnodes;
+       cout<<"\nThe number of leaves are "<<b.leaves;
+       cout<<"\nThe number of internal nodes are "<<b.noofnodes-b.leaves;break;
+  deafult:cout<<"\ntype 1,2,3,4,5,6 or 7 to exit ";break;
+  }
+}while(ch!=7);
+return 0;
 }
